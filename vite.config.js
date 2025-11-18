9f9868d6a33a1345dce1810bf9f70d83962592b0
mahalakshmi-207 <mahalakshmibalamurugan70@gmail.com>
2025-11-18 12:34:20 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,16 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
   server: {
     proxy: {
-      "/api": {
-        target: "http://localhost:5000",
+      '/api': {
+        target: 'http://localhost:5000',
         changeOrigin: true,
         secure: false,
       },
     },
   },
-});
+})
