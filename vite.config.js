7b0da33bf06fa15ee5e3b36a37140716552fa48b
mahalakshmi-207 <mahalakshmibalamurugan70@gmail.com>
2025-11-18 13:01:42 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  root: 'src', 
-  build: {
-    outDir: '../dist', 
-    emptyOutDir: true, 
-  },
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000', //  backend server
+        target: 'http://localhost:5000',
         changeOrigin: true,
         secure: false,
       },
